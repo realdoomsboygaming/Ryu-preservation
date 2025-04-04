6cee5a50c9e52cd7985ccfabcfb27d6e07c6f0f8
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-04-04 10:17:30 -0500

    Update SettingsViewSources.swift

--- Ryu/Views/Settings/SubPages/SettingsViewSources.swift
+++ Ryu/Views/Settings/SubPages/SettingsViewSources.swift
@@ -1,48 +1,52 @@
-//
-//  SettingsViewSources.swift
-//  Ryu
-//
-//  Created by Francesco on 03/08/24.
-//
-
 import UIKit
 
 class SettingsViewSources: UITableViewController {
-    
+
     @IBOutlet weak var retryMethod: UIButton!
     @IBOutlet weak var qualityPrefered: UIButton!
-    
+
     @IBOutlet weak var gogoButton: UIButton!
-    
-    @IBOutlet weak var audioButton: UIButton!
-    @IBOutlet weak var serverButton: UIButton!
-    @IBOutlet weak var subtitlesButton: UIButton!
-    
+
+    // Renamed outlets for clarity (previously related to HiAnime)
+    @IBOutlet weak var anilistAudioButton: UIButton!
+    @IBOutlet weak var anilistServerButton: UIButton!
+    @IBOutlet weak var anilistSubtitlesButton: UIButton!
+
     @IBOutlet weak var otherFOrmats: UISwitch!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupRetryMenu()
         setupMenu()
         setupGoGo()
-        setupAudioMenu()
-        setupServerMenu()
-        setupSubtitlesMenu()
+        setupAniListAudioMenu() // Renamed function
+        setupAniListServerMenu() // Renamed function
+        setupAniListSubtitlesMenu() // Renamed function
         loadUserDefaults()
-        
+
         if let selectedOption = UserDefaults.standard.string(forKey: "preferredQuality") {
             qualityPrefered.setTitle(selectedOption, for: .normal)
         }
     }
-    
+
     private func loadUserDefaults() {
         otherFOrmats.isOn = UserDefaults.standard.bool(forKey: "otherFormats")
+        // Load AniList specific preferences
+        if let audioPref = UserDefaults.standard.string(forKey: "anilistAudioPref") { // Updated key
+            anilistAudioButton.setTitle(audioPref, for: .normal)
+        }
+        if let serverPref = UserDefaults.standard.string(forKey: "anilistServerPref") { // Updated key
+            anilistServerButton.setTitle(serverPref, for: .normal)
+        }
+        if let subtitlePref = UserDefaults.standard.string(forKey: "anilistSubtitlePref") { // Updated key
+            anilistSubtitlesButton.setTitle(subtitlePref, for: .normal)
+        }
     }
-    
+
     @IBAction func otherFormatsToggle(_ sender: UISwitch) {
         UserDefaults.standard.set(sender.isOn, forKey: "otherFormats")
     }
-    
+
     func setupRetryMenu() {
         let actions = [
             UIAction(title: "5 Tries", handler: { [weak self] _ in
@@ -61,19 +65,19 @@ class SettingsViewSources: UITableViewController {
                 self?.setRetries(25)
             })
         ]
-        
+
         let menu = UIMenu(title: "Select Retry Count", children: actions)
-        
+
         retryMethod.menu = menu
         retryMethod.showsMenuAsPrimaryAction = true
-        
+
         if let retries = UserDefaults.standard.value(forKey: "maxRetries") as? Int {
             retryMethod.setTitle("\(retries) Tries", for: .normal)
         } else {
-            retryMethod.setTitle("Select Tries", for: .normal)
+            retryMethod.setTitle("Select Tries", for: .normal) // Default or placeholder
         }
     }
-    
+
     func setupGoGo() {
         let action1 = UIAction(title: "Default", handler: { [weak self] _ in
             UserDefaults.standard.set("Default", forKey: "gogoFetcher")
@@ -83,22 +87,22 @@ class SettingsViewSources: UITableViewController {
             UserDefaults.standard.set("Secondary", forKey: "gogoFetcher")
             self?.gogoButton.setTitle("Secondary", for: .normal)
         })
-        
+
         let menu = UIMenu(title: "Select Prefered Method", children: [action1, action2])
-        
+
         gogoButton.menu = menu
         gogoButton.showsMenuAsPrimaryAction = true
-        
+
         if let selectedOption = UserDefaults.standard.string(forKey: "gogoFetcher") {
             gogoButton.setTitle(selectedOption, for: .normal)
         }
     }
-    
+
     private func setRetries(_ retries: Int) {
         UserDefaults.standard.set(retries, forKey: "maxRetries")
         retryMethod.setTitle("\(retries) Tries", for: .normal)
     }
-    
+
     func setupMenu() {
         let action1 = UIAction(title: "360p", handler: { [weak self] _ in
             UserDefaults.standard.set("360p", forKey: "preferredQuality")
@@ -116,97 +120,109 @@ class SettingsViewSources: UITableViewController {
             UserDefaults.standard.set("1080p", forKey: "preferredQuality")
             self?.qualityPrefered.setTitle("1080p", for: .normal)
         })
-        
+
         let menu = UIMenu(title: "Select Prefered Quality", children: [action1, action2, action3, action4])
-        
+
         qualityPrefered.menu = menu
         qualityPrefered.showsMenuAsPrimaryAction = true
-        
+
         if let selectedOption = UserDefaults.standard.string(forKey: "preferredQuality") {
             qualityPrefered.setTitle(selectedOption, for: .normal)
         }
     }
-    
-    func setupAudioMenu() {
+
+    // Renamed function
+    func setupAniListAudioMenu() {
         let action1 = UIAction(title: "sub", handler: { [weak self] _ in
-            UserDefaults.standard.set("sub", forKey: "audioHiPrefe")
-            self?.audioButton.setTitle("sub", for: .normal)
+            UserDefaults.standard.set("sub", forKey: "anilistAudioPref") // Updated key
+            self?.anilistAudioButton.setTitle("sub", for: .normal)
         })
         let action2 = UIAction(title: "dub", handler: { [weak self] _ in
-            UserDefaults.standard.set("dub", forKey: "audioHiPrefe")
-            self?.audioButton.setTitle("dub", for: .normal)
+            UserDefaults.standard.set("dub", forKey: "anilistAudioPref") // Updated key
+            self?.anilistAudioButton.setTitle("dub", for: .normal)
         })
         let action3 = UIAction(title: "raw", handler: { [weak self] _ in
-            UserDefaults.standard.set("raw", forKey: "audioHiPrefe")
-            self?.audioButton.setTitle("raw", for: .normal)
+            UserDefaults.standard.set("raw", forKey: "anilistAudioPref") // Updated key
+            self?.anilistAudioButton.setTitle("raw", for: .normal)
         })
         let action4 = UIAction(title: "Always Ask", handler: { [weak self] _ in
-            UserDefaults.standard.set("Always Ask", forKey: "audioHiPrefe")
-            self?.audioButton.setTitle("Always Ask", for: .normal)
+            UserDefaults.standard.set("Always Ask", forKey: "anilistAudioPref") // Updated key
+            self?.anilistAudioButton.setTitle("Always Ask", for: .normal)
         })
-        
+
         let menu = UIMenu(title: "Select Prefered Audio", children: [action1, action2, action3, action4])
-        
-        audioButton.menu = menu
-        audioButton.showsMenuAsPrimaryAction = true
-        
-        if let selectedOption = UserDefaults.standard.string(forKey: "audioHiPrefe") {
-            audioButton.setTitle(selectedOption, for: .normal)
+
+        anilistAudioButton.menu = menu // Use renamed outlet
+        anilistAudioButton.showsMenuAsPrimaryAction = true
+
+        if let selectedOption = UserDefaults.standard.string(forKey: "anilistAudioPref") { // Updated key
+            anilistAudioButton.setTitle(selectedOption, for: .normal)
+        } else {
+            anilistAudioButton.setTitle("Always Ask", for: .normal) // Set default if none saved
+            UserDefaults.standard.set("Always Ask", forKey: "anilistAudioPref")
         }
     }
-    
-    func setupServerMenu() {
+
+    // Renamed function
+    func setupAniListServerMenu() {
         let action1 = UIAction(title: "hd-1", handler: { [weak self] _ in
-            UserDefaults.standard.set("hd-1", forKey: "serverHiPrefe")
-            self?.serverButton.setTitle("hd-1", for: .normal)
+            UserDefaults.standard.set("hd-1", forKey: "anilistServerPref") // Updated key
+            self?.anilistServerButton.setTitle("hd-1", for: .normal)
         })
         let action2 = UIAction(title: "hd-2", handler: { [weak self] _ in
-            UserDefaults.standard.set("hd-2", forKey: "serverHiPrefe")
-            self?.serverButton.setTitle("hd-2", for: .normal)
+            UserDefaults.standard.set("hd-2", forKey: "anilistServerPref") // Updated key
+            self?.anilistServerButton.setTitle("hd-2", for: .normal)
         })
         let action3 = UIAction(title: "Always Ask", handler: { [weak self] _ in
-            UserDefaults.standard.set("Always Ask", forKey: "serverHiPrefe")
-            self?.serverButton.setTitle("Always Ask", for: .normal)
+            UserDefaults.standard.set("Always Ask", forKey: "anilistServerPref") // Updated key
+            self?.anilistServerButton.setTitle("Always Ask", for: .normal)
         })
-        
+
         let menu = UIMenu(title: "Select Prefered Server", children: [action1, action2, action3])
-        
-        serverButton.menu = menu
-        serverButton.showsMenuAsPrimaryAction = true
-        
-        if let selectedOption = UserDefaults.standard.string(forKey: "serverHiPrefe") {
-            serverButton.setTitle(selectedOption, for: .normal)
+
+        anilistServerButton.menu = menu // Use renamed outlet
+        anilistServerButton.showsMenuAsPrimaryAction = true
+
+        if let selectedOption = UserDefaults.standard.string(forKey: "anilistServerPref") { // Updated key
+            anilistServerButton.setTitle(selectedOption, for: .normal)
+        } else {
+            anilistServerButton.setTitle("Always Ask", for: .normal) // Set default if none saved
+            UserDefaults.standard.set("Always Ask", forKey: "anilistServerPref")
         }
     }
-    
-    func setupSubtitlesMenu() {
+
+    // Renamed function
+    func setupAniListSubtitlesMenu() {
         let action1 = UIAction(title: "English", handler: { [weak self] _ in
-            UserDefaults.standard.set("English", forKey: "subtitleHiPrefe")
-            self?.subtitlesButton.setTitle("English", for: .normal)
+            UserDefaults.standard.set("English", forKey: "anilistSubtitlePref") // Updated key
+            self?.anilistSubtitlesButton.setTitle("English", for: .normal)
         })
         let action2 = UIAction(title: "Always Ask", handler: { [weak self] _ in
-            UserDefaults.standard.set("Always Ask", forKey: "subtitleHiPrefe")
-            self?.subtitlesButton.setTitle("Always Ask", for: .normal)
+            UserDefaults.standard.set("Always Ask", forKey: "anilistSubtitlePref") // Updated key
+            self?.anilistSubtitlesButton.setTitle("Always Ask", for: .normal)
         })
         let action3 = UIAction(title: "No Subtitles", handler: { [weak self] _ in
-            UserDefaults.standard.set("No Subtitles", forKey: "subtitleHiPrefe")
-            self?.subtitlesButton.setTitle("No Subtitles", for: .normal)
+            UserDefaults.standard.set("No Subtitles", forKey: "anilistSubtitlePref") // Updated key
+            self?.anilistSubtitlesButton.setTitle("No Subtitles", for: .normal)
         })
         let action4 = UIAction(title: "Always Import", handler: { [weak self] _ in
-            UserDefaults.standard.set("Always Import", forKey: "subtitleHiPrefe")
-            self?.subtitlesButton.setTitle("Always Import", for: .normal)
+            UserDefaults.standard.set("Always Import", forKey: "anilistSubtitlePref") // Updated key
+            self?.anilistSubtitlesButton.setTitle("Always Import", for: .normal)
         })
-        
+
         let menu = UIMenu(title: "Select Subtitles Language", children: [action1, action3, action4, action2])
-        
-        subtitlesButton.menu = menu
-        subtitlesButton.showsMenuAsPrimaryAction = true
-        
-        if let selectedOption = UserDefaults.standard.string(forKey: "subtitleHiPrefe") {
-            subtitlesButton.setTitle(selectedOption, for: .normal)
+
+        anilistSubtitlesButton.menu = menu // Use renamed outlet
+        anilistSubtitlesButton.showsMenuAsPrimaryAction = true
+
+        if let selectedOption = UserDefaults.standard.string(forKey: "anilistSubtitlePref") { // Updated key
+            anilistSubtitlesButton.setTitle(selectedOption, for: .normal)
+        } else {
+            anilistSubtitlesButton.setTitle("English", for: .normal) // Set default if none saved
+            UserDefaults.standard.set("English", forKey: "anilistSubtitlePref")
         }
     }
-    
+
     @IBAction func closeButtonTapped() {
         dismiss(animated: true, completion: nil)
     }
